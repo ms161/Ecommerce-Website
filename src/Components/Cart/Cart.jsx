352d20d4341566356c789df808070bb5de31164d
ms161 <msehrawat161>
2023-06-22 15:17:15 +0530

    cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext } from "react";
 import CarrtItems from "./CarrtItems";
 import AuthContext from "../Store/Auth-Context";
@@ -23,33 +22,3 @@ const Cart = (props) => {
 };
 
 export default Cart;
-=======
-import React from 'react'
-import CarrtItems from './CarrtItems'
-
-const Cart = (props) => {
-
-    const showCartHandler=e=>{
-
-    }
-
-  return (
-   <>
-   <div>
-    <div>
-   
-    </div>
-    <div  >
-        <CarrtItems onClick={showCartHandler} data={props.data}></CarrtItems>
-       
-    </div>
-    <div >
-      
-    </div>
-   </div>
-   </>
-  )
-}
-
-export default Cart
->>>>>>> 13db6a03115596c0046f37110c5a3a2aa33cb2a5
--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-<<<<<<< HEAD
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-<App />
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
-);
-=======
+// root.render(
+// <App />
+
+// );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
->>>>>>> 13db6a03115596c0046f37110c5a3a2aa33cb2a5
+
